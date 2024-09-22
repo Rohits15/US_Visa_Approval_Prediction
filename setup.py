1bf41d8f01093effafc54e787d304eb19d672740
Rohit Sahu <rohit.sahu.j5@gmail.com>
2024-09-22 16:52:02 +0530

    Requirenemts added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Rohits15",
+    author_email="rohit.sahu.j5@gmail.com",
     packages=find_packages()
 )
